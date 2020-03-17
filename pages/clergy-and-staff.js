f36d742ad1bda3e7f07f62b9562fbe50d4fc8e34
jon mcgill <jon.d.mcgill@gmail.com>
2020-03-17 09:22:03 -0400

    update vestry list

--- pages/clergy-and-staff.js
+++ pages/clergy-and-staff.js
@@ -1,6 +1,5 @@
 import Page from '../components/Page';
 import PageContent from '../components/PageContent';
-import Space from '../components/Space';
 
 export default function ClergyAndStaff() {
   return (
@@ -8,11 +7,24 @@ export default function ClergyAndStaff() {
       <PageContent>
         <h2>Bishop</h2>
         <p>
-          <img src="/img/bp-manto.png" alt="Peter Manto, Bishop Ordinary" />The Rt. Rev. Peter Manto is rector of Trinity Church and Bishop Ordinary of the REC's Diocese of the Central States. Bp. Manto has been a parish pastor for over 40 years. He was the church-planter for the non-denominational church which became Trinity Church and guided its move into the Reformed Episcopal Church. Bp. Manto has been married to his wife Janice for over 40 years. They have four adult children and 13 grandchildren.
+          <img src="/img/bp-manto.png" alt="Peter Manto, Bishop Ordinary" />The Rt. Rev. Peter Manto
+          is rector of Trinity Church and Bishop Ordinary of the REC's Diocese of the Central States. Bp.
+          Manto has been a parish pastor for over 40 years. He was the church-planter for the
+          non-denominational church which became Trinity Church and guided its move into the Reformed
+          Episcopal Church. Bp. Manto has been married to his wife Janice for over 40 years. They have
+          four adult children and 13 grandchildren.
         </p>
         <h2>Parish Rector</h2>
         <p>
-          <img src="/img/fr-wayne.png" alt="Wayne McNamara, Parish Rector" />Fr. Wayne became rector of Christ the King in 1992 after 12 years of serving in special education. He was ordained in 1998 by the Association of Independent Covenant Churches and was received as a Presbyter in the Reformed Episcopal Church in 2000. He currently serves on the boards of the Children’s Hunger Relief Fund, St. Lazarus Mission, and the Huffman Historic Neighborhood Association. In 1998, Fr. Wayne co-founded Dominion Academy of Dayton where he serves as Priest-in-Charge. His scholarly interests lie in liturgics, Anglican studies, and the Christian ascetical tradition, and in 2015, he was accepted as an oblate by the Order of Saint Benedict. Fr. Wayne has four children and four grandchildren and lives with his wife, Sandy.
+          <img src="/img/fr-wayne.png" alt="Wayne McNamara, Parish Rector" />Fr. Wayne became rector
+          of Christ the King in 1992 after 12 years of serving in special education. He was ordained
+          in 1998 by the Association of Independent Covenant Churches and was received as a Presbyter
+          in the Reformed Episcopal Church in 2000. He currently serves on the boards of the Children's
+          Hunger Relief Fund, St. Lazarus Mission, and the Huffman Historic Neighborhood Association. In
+          1998, Fr. Wayne co-founded Dominion Academy of Dayton where he serves as Priest-in-Charge. His
+          scholarly interests lie in liturgics, Anglican studies, and the Christian ascetical tradition,
+          and in 2015, he was accepted as an oblate by the Order of Saint Benedict. Fr. Wayne has four
+          children and four grandchildren and lives with his wife, Sandy.
         </p>
         <h2>Parish Staff</h2>
         <ul>
@@ -35,6 +47,7 @@ export default function ClergyAndStaff() {
           <li>Deepika McNamara</li>
           <li>Tim Dana</li>
           <li>James Hoffman</li>
+          <li>Lauren Hilyer</li>
         </ul>
         <style jsx>{`
           img {
