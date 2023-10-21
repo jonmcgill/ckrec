fbfd4a2c2966a9c62562a229f4ba77353e1821a4
Daschel Fortner <daschfortner@gmail.com>
2023-10-21 15:34:01 -0400

    Added the navigation bar to the pages

--- components/Page.js
+++ components/Page.js
@@ -1,10 +1,12 @@
 import PageBackground from './PageBackground';
 import PageHeading from './PageHeading';
 import Footer from './Footer';
+import NavigationBar from './NavigationBar';
 
 export default function Page({ children, isHome, title }) {
   return (
     <>
+      <NavigationBar />
       <PageBackground />
       <PageHeading isHome={isHome} title={title} />
       {children}
